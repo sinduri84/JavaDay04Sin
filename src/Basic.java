e2f7c2244902bf6e49428a7869f5afc6ed94ad06
Sinduri Guntupalli <sinduri.g@gmail.com>
2020-07-02 09:26:07 +0200

    Basic Commit

--- src/Basic.java
+++ src/Basic.java
@@ -1,2 +1,27 @@
 public class Basic {
+
+    static void printNumbers(int aNumber) {
+        for(int i = 0; i < aNumber; i++) {
+            System.out.println(i + 1);
+        }
+    }
+
+    static void printEvenNumbers(int aNumber) {
+        for (int i = 0; i <= aNumber; i+=2) {
+            if (i == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        //A1 Exercise
+        System.out.println("A1 Exercise\n");
+        printNumbers(100);
+
+        //A2 Exercise
+        System.out.println("A2 Exercise\n");
+        printEvenNumbers(100);
+
+    }
 }
--- src/Intermediate.java
+++ src/Intermediate.java
@@ -1,2 +1,7 @@
 public class Intermediate {
+
+
+    public static void main(String[] args) {
+
+    }
 }
